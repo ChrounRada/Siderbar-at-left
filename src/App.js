681cd789ee9708145c286632c252c025537d8c94
Chroun Rada <67093871+ChrounRada@users.noreply.github.com>
2021-01-28 16:10:04 +0700

    clash free time table

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, {useEffect, useState} from 'react';
+// import { FaAngleDoubleRight } from 'react-icons/fa' 
+
+const url = 'https://course-api.com/react-tabs-project'
+
+function App() {
+
+  const [loading, setloading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const reponse = await fetch(url)
+    const newJobs = await reponse.json()
+    setJobs(newJobs)
+    setloading(false)
+  } 
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+
+  if (loading){
+    return (
+      <section className='section loading'>
+        <h2>loading....</h2>
+      </section>
+    )
+  }
+
+  const {company, dates, duties, title} = jobs[value]
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>Experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+      { /* btn-container */ }
+        <div className='btn-container'>
+        {jobs.map((item, index)=> {
+        return(
+          <button
+            key={item.id}
+            onClick={() => setValue(index)}
+            className={`job-btn ${index=== value && 'active-btn'}`}
+          >
+          {item.company}
+          </button>
+        )
+      })}
+        </div>
+      { /* Jobs info */ }
+      <article className='job-info'>
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className='job-date'>{dates}</p>
+        {duties.map((duty, index) => {
+          return (
+            <div key={index} className='job-desc'>
+            {/* <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight> */}
+            <p>{duty}</p>
+            </div>
+          )
+        })}
+      </article>
+      </div>
+      <button type='button' className='btn'>
+        More info
+      </button>
+    </section>
+    
+  );
+}
+
+export default App;
